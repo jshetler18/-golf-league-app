60175e166e5995cdeb5a756b317eccb2d1497b47
jshetler18 <joeshetler@ymail.com>
2026-09-20 10:05:08 -0400

    Add Kotlin plugin and update app version
    Added Kotlin plugin and updated version code and name.

--- fire-tv-app/app/build.gradle.kts
+++ fire-tv-app/app/build.gradle.kts
@@ -1,13 +1,17 @@
-plugins { id("com.android.application") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
 android {
     namespace = "com.lvvgolfsim.leaderboard"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.lvvgolfsim.leaderboard"
         minSdk = 28
         targetSdk = 28
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 }
